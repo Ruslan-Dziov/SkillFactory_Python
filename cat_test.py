ca08c43a87c6d64dea897de5ae621cce103c5273
Ruslan-Dziov <dziovruslan@gmail.com>
2021-05-01 18:39:34 +0300

    First commit

---
+++ cat_test.py
@@ -0,0 +1,11 @@
+from cat import Cat
+
+cat1 = Cat('Барон', 'мальчик', '2 года')
+cat2 = Cat('Сэм', 'мальчик', '2 года')
+
+print('кличка кота', cat1.get_name())
+print('возраст кота', cat1.get_age())
+print('пол кота', cat1.get_sex())
+print('кличка кота', cat2.get_name())
+print('возраст кота', cat2.get_age())
+print('пол кота', cat2.get_sex())
